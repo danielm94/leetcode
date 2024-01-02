654e8dc59e23c2cc764691252b4c8423f990ba4a
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-02 00:01:47 -0500

    Initial commit of AnagramGrouper

---
+++ src/main/java/com/github/danielm94/medium/AnagramGrouper.java
@@ -0,0 +1,25 @@
+package com.github.danielm94.medium;
+
+import lombok.val;
+
+import java.util.*;
+
+public class AnagramGrouper {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        var solution = new ArrayList<List<String>>();
+        if (strs.length == 0) return solution;
+        var map = new HashMap<String, List<String>>();
+        for (var string : strs) {
+            var charArr = string.toCharArray();
+            Arrays.sort(charArr);
+            var sortedString = new String(charArr);
+            map.computeIfAbsent(sortedString, k -> new ArrayList<>());
+            var list = map.get(sortedString);
+            list.add(string);
+        }
+        for (var key : map.keySet()) {
+            solution.add(map.get(key));
+        }
+        return solution;
+    }
+}
---
+++ src/test/java/com/github/danielm94/medium/AnagramGrouperTest.java
@@ -0,0 +1,68 @@
+package com.github.danielm94.medium;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnagramGrouperTest {
+    private AnagramGrouper anagramGrouper;
+
+    @BeforeEach
+    void setUp() {
+        this.anagramGrouper = new AnagramGrouper();
+    }
+
+    private boolean areEqualIgnoringOrder(List<List<String>> list1, List<List<String>> list2) {
+        if (list1.size() != list2.size()) return false;
+
+        Set<Set<String>> set1 = new HashSet<>();
+        for (List<String> sublist : list1) {
+            set1.add(new HashSet<>(sublist));
+        }
+
+        Set<Set<String>> set2 = new HashSet<>();
+        for (List<String> sublist : list2) {
+            set2.add(new HashSet<>(sublist));
+        }
+
+        return set1.equals(set2);
+    }
+
+    @Test
+    public void testExample1() {
+        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> expected = Arrays.asList(
+                Arrays.asList("bat"),
+                Arrays.asList("nat", "tan"),
+                Arrays.asList("ate", "eat", "tea")
+        );
+
+        assertTrue(areEqualIgnoringOrder(expected, anagramGrouper.groupAnagrams(input)));
+    }
+
+    @Test
+    public void testExample2() {
+        String[] input = {""};
+        List<List<String>> expected = Arrays.asList(
+                Arrays.asList("")
+        );
+
+        assertTrue(areEqualIgnoringOrder(expected, anagramGrouper.groupAnagrams(input)));
+    }
+
+    @Test
+    public void testExample3() {
+        String[] input = {"a"};
+        List<List<String>> expected = Arrays.asList(
+                Arrays.asList("a")
+        );
+
+        assertTrue(areEqualIgnoringOrder(expected, anagramGrouper.groupAnagrams(input)));
+    }
+}
