fcfbec526e574361c636faccbaf116722a479594
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-22 00:45:33 -0500

    Add toString() method for debugging ListNode

--- src/main/java/com/github/danielm94/datastructures/linkedlist/ListNode.java
+++ src/main/java/com/github/danielm94/datastructures/linkedlist/ListNode.java
@@ -1,5 +1,7 @@
 package com.github.danielm94.datastructures.linkedlist;
 
+import java.util.HashSet;
+
 public class ListNode {
     public int val;
     public ListNode next;
@@ -14,5 +16,22 @@ public class ListNode {
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        var builder = new StringBuilder();
+        builder.append(val);
+        var cycleDetector = new HashSet<ListNode>();
+        var cycleFound = false;
+        for (var curr = next; curr != null; curr = curr.next) {
+            if (!cycleDetector.add(curr)) {
+                cycleFound = true;
+                break;
+            }
+            builder.append(" > ").append(curr.val);
+        }
+        if (cycleFound) builder.append(" > infinite cycle..");
+        return builder.toString();
     }
 }
